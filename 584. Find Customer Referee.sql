23373abdeae9945f81477c622ce6abf670f9c001
M. SIVARAJ <msivaraj172@gmail.com>
2026-07-31 06:59:30 +0530

    Implement query to find customer referees
    Add SQL query to find customers without a specific referee.

---
+++ 584. Find Customer Referee.sql
@@ -0,0 +1,32 @@
+'''584. Find Customer Referee
+
+Example 1:
+
+Input: 
+Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+Output: 
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+'''
+
+
+#query link : https://leetcode.com/problems/find-customer-referee/description/?envType=study-plan-v2&envId=top-sql-50
+
+
+
+select name from Customer where referee_id != 2 or referee_id is null
