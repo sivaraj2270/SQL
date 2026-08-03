6ec88eb1b5985e5839ab24af7edc3610231b35c9
M. SIVARAJ <msivaraj172@gmail.com>
2026-08-03 07:04:54 +0530

    Implement query for Not Boring Movies
    Add SQL query to select non-boring movies with odd IDs.

---
+++ 620. Not Boring Movies.sql
@@ -0,0 +1,32 @@
+'''620. Not Boring Movies
+
+Example 1:
+
+Input: 
+Cinema table:
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 1  | War        | great 3D    | 8.9    |
+| 2  | Science    | fiction     | 8.5    |
+| 3  | irish      | boring      | 6.2    |
+| 4  | Ice song   | Fantacy     | 8.6    |
+| 5  | House card | Interesting | 9.1    |
++----+------------+-------------+--------+
+Output: 
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 5  | House card | Interesting | 9.1    |
+| 1  | War        | great 3D    | 8.9    |
++----+------------+-------------+--------+
+Explanation: 
+We have three movies with odd-numbered IDs: 1, 3, and 5. The movie with ID = 3 is boring so we do not include it in the answer.'''
+
+
+
+#query link :"https://leetcode.com/problems/not-boring-movies/?envType=study-plan-v2&envId=top-sql-50"
+
+SELECT id, movie, description, rating FROM Cinema WHERE id % 2 <> 0 AND description <> 'boring'
+ORDER BY rating DESC;
+
